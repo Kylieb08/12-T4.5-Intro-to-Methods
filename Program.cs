cebebfc2c77abb8f7ed5ad0915e2a947aa54b9c8
339780520 <339780520@2260I-L29-008>
2025-09-19 09:07:44 -0400

    done

--- Program.cs
+++ Program.cs
@@ -4,9 +4,33 @@
     {
         static void Main(string[] args)
         {
+            int bugs;
+            string anotherJoke;
+
             Console.WriteLine("Press ENTER for a joke:");
             Console.ReadLine();
             Joke(); //This "calls" or "invokes" the method Joke()
+            Console.WriteLine();
+            Console.WriteLine("Would you like to hear another joke?");
+            anotherJoke = Console.ReadLine();
+
+            if (anotherJoke.ToLower() == "yes" || anotherJoke.ToLower() == "y")
+            {
+                Console.WriteLine("How many bugs are in your code?");
+                Int32.TryParse(Console.ReadLine(), out bugs);
+                Joke(bugs);
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("oh");
+                Thread.Sleep(1000);
+                Console.WriteLine("ok");
+                Thread.Sleep(1000);
+                Console.WriteLine("Goodbye");
+            }
         }
 
         public static void Joke()
@@ -18,6 +42,18 @@
             Console.WriteLine("Fix a bug, run it again,");
             Thread.Sleep(500);
             Console.WriteLine("100 little bugs in the code.");
+        }
+
+        public static void Joke(int numBugs)
+        {
+            Console.WriteLine();
+            Console.WriteLine(numBugs + " little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine(numBugs + " little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine((++numBugs) + " little bugs in the code.");
         }
     }
 }
