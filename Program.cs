e0915f59f732683187dc937d6db9d811c7e114dd
339780520 <339780520@2260I-L29-008>
2025-09-19 08:39:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_T4._5_Intro_to_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
