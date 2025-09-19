d7596fa1f04b4fb31c1723d58cbde30baa2f023c
339780520 <339780520@2260I-L29-008>
2025-09-19 08:51:57 -0400

    added Joke method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Press ENTER for a joke:");
+            Console.ReadLine();
+            Joke(); //This "calls" or "invokes" the method Joke()
         }
 
-
+        public static void Joke()
+        {
+            Console.WriteLine("99 little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine("99 little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine("100 little bugs in the code.");
+        }
     }
 }
